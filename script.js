ba0add1eb2ad51a6b35f8de400b9d7207ab1d916
Eduardo Ezequiel Gomez <edu.ezequielgomez@gmail.com>
2024-11-16 21:14:54 -0300

    Version 0.4.1

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
 const pkaspa = "https://api.kaspa.org/info/price?stringOnly=true";
 let dato;
 
-//valor agregado
-const valorObjetivo = 0.1715; 
-const botToken = '7025151223:AAEpZ-ga8wmCd5tsCGgh_zhiMmoyIuOKUIs'; // Tu token de bot de Telegram
-const chatId = '1346927197'; // Tu chat ID
-
 const ppkaspa = async() => {
     const res = await fetch(pkaspa);
     dato = await res.json()
@@ -28,19 +23,6 @@ function ejecutar(evento){
     let cantidadKaspa = document.getElementById("ckaspa").value
     let resultado = document.getElementById("resultado")
     let cuenta = dato * cantidadKaspa;
-    resultado.innerHTML = `<p>$${Math.trunc(cuenta)}</p>`
-}
-
-async function enviarMensajeTelegram() {
-    const message = `El valor de Kaspa ha alcanzado el objetivo de ${valorObjetivo}.`;
-    const url = `https://api.telegram.org/bot${botToken}/sendMessage?chat_id=${chatId}&text=${encodeURIComponent(message)}`;
-
-    try {
-        const response = await fetch(url);
-        const result = await response.json();
-        console.log('Mensaje enviado:', result);
-    } catch (error) {
-        console.error('Error al enviar el mensaje:', error);
-    }
+    resultado.innerHTML = `<p>$${Math.trunc(cuenta)}.-</p>`
 }
 
