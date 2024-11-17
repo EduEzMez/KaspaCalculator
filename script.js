3e068143cec8205c977b0702cbd28263a8c845a3
Eduardo Ezequiel Gomez <edu.ezequielgomez@gmail.com>
2024-11-16 22:41:01 -0300

    4.2

--- script.js
+++ script.js
@@ -1,19 +1,16 @@
-const pkaspa = "https://api.kaspa.org/info/price?stringOnly=true";
+const API_KASPA = "https://api.kaspa.org/info/price?stringOnly=true";
 let dato;
 
-const ppkaspa = async() => {
-    const res = await fetch(pkaspa);
-    dato = await res.json()
-
+const VALOR_KASPA = async() => {
+    const RESULTADO = await fetch(API_KASPA);
+    dato = await RESULTADO.json()
+    //console.log(dato)
     let valorKaspa = document.getElementById("vkaspa")
     valorKaspa.innerHTML = dato;
-
-    if (dato == valorObjetivo) {
-        enviarMensajeWhatsApp();
-    }
 };
-ppkaspa()
-setInterval(ppkaspa, 3000)
+
+VALOR_KASPA()
+setInterval(VALOR_KASPA, 3000)
 
 const bton = document.getElementById('btn')
 bton.addEventListener('click', ejecutar)
